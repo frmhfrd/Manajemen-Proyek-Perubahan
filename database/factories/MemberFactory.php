f8f91dc54f9c37004acf26c2a8cec49c9308e5ab
frmhfrd <fahrimuhamadfirdaus17@gmail.com>
2026-01-22 08:38:29 +0700

    Memberseed

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -16,15 +16,32 @@ class MemberFactory extends Factory
      */
     public function definition(): array
     {
+        // 1. DAFTAR TEMAN ANDA
+        $demoContacts = [
+            ['name' => 'Rafael Pransa',         'hp' => '081313099878'],
+            ['name' => 'Fahri Muhamad Firdaus', 'hp' => '085117047720'],
+            ['name' => 'Aril Awali',            'hp' => '087898117703'],
+            ['name' => 'Dafa Wafi Pradipa',     'hp' => '089506291487'],
+            ['name' => 'Mochamad Dhafin Azriya','hp' => '082123703102'],
+        ];
+
+        // 2. Pilih 1 orang secara acak
+        $friend = fake()->randomElement($demoContacts);
+
         return [
-            'kode_anggota' => fake()->unique()->numerify('2024####'), // NIS contoh: 20240001
-            'nama_lengkap' => fake()->name(),
+            // --- DATA OTOMATIS (Tetap Ada) ---
+            'kode_anggota' => fake()->unique()->numerify('2024####'), // Contoh: 20249912
             'tipe_anggota' => fake()->randomElement(['siswa', 'guru']),
             'kelas'        => fake()->randomElement(['1A', '2B', '3A', '4C', '5B', '6A']),
-            'wali_kelas'   => fake()->name(),
-            'no_telepon'   => fake()->phoneNumber(),
             'alamat'       => fake()->address(),
             'status_aktif' => true,
+
+            // --- DATA TEMAN (Biar Demo Keren) ---
+            'nama_lengkap' => $friend['name'], // Nama Member jadi nama teman
+            'no_telepon'   => $friend['hp'],   // HP Member jadi HP teman
+
+            // Wali kelas kita isi nama acak saja (atau nama orang tua teman)
+            'wali_kelas'   => fake()->name(),
         ];
     }
 }
